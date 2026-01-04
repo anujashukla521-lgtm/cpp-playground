fe237c8da8a9b2408863301dab84bef9bafe6143
anujashukla521-lgtm <anujashukla521@gmail.com>
2026-01-04 19:53:06 +0530

    Code 4

--- code4.cpp
+++ code4.cpp
@@ -1,95 +1,28 @@
-//Program of multilevel inheritance calculating total marks and grade of a student
+// A basic C++ program showing how function overloading works. The program uses three versions of the area() function to compute the area of different shapes.
 
 #include <iostream>
-#include <string>
 using namespace std;
+const float PI = 3.14;
 
-class Student
+double area(double r)
 {
-    int roll_no;
-    string name;
-
-public:
-    void setRollNO(void)
-    {
-        cout << "Enter roll no of student:" << endl;
-        cin >> roll_no;
-    }
-    int getRollNo()
-    {
-        return roll_no;
-    }
-
-    void setName(void)
-    {
-        cout << "Enter name of student:" << endl;
-        cin.ignore();
-        getline(cin, name);
-    }
-    string getName()
-    {
-        return name;
-    }
-};
-class InternalMarks : public Student
+    cout << "Area of circle: " << PI * r * r << endl;
+}
+double area(double length, double breadth)
 {
-    float m1, m2, m3;
+    cout << "Area of rectangle: " << length * breadth << endl;
+}
 
-public:
-    void setMarks(void)
-    {
-        cout << "Enter marks in 3 subjects" << endl;
-        cin >> m1 >> m2 >> m3;
-    }
-    float getMarks()
-    {
-        return m1 + m2 + m3;
-    }
-};
-class FinalResult : public InternalMarks
+double area(int base, int height)
 {
-protected:
-    float total;
-    char grade;
+    cout << "Area of triangle: " << 0.5 * base * height << endl;
+}
 
-public:
-    void gradeCheck()
-    {
-        total = getMarks();
-        if (total >= 90)
-        {
-            grade = 'A';
-        }
-        else if (total >= 75)
-        {
-            grade = 'B';
-        }
-        else if (total >= 50)
-        {
-            grade = 'C';
-        }
-        else
-        {
-            grade = 'D';
-        }
-    }
-    void display()
-    {
-        gradeCheck();
-        cout << "\n\n----------Student's Detail-----------" << endl;
-        cout << "Roll number of the student: " << getRollNo() << endl;
-        cout << "Name of the student: " << getName() << endl;
-        cout << "Total Marks of the student: " << total << endl;
-        cout << "Grade of the student: " << grade << endl;
-    }
-};
 int main()
 {
+    area(5);
+    area(12.2, 3.5);
+    area(12, 7);
 
-    FinalResult f;
-    f.setRollNO();
-    f.setName();
-    f.setMarks();
-    f.display();
     return 0;
 }
