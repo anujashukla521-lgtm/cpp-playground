a5841d9cd286f6761e1d7a075accce69a2dcb421
anujashukla521-lgtm <anujashukla521@gmail.com>
2026-01-20 17:00:42 +0530

    Update write_read.cpp

--- File_io/basic_write_read/write_read.cpp
+++ File_io/basic_write_read/write_read.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+    // opening file using constructor and writing to it
+    string st = "Hey!!";
+    ofstream out("demo1.txt");  //write operation
+    out<<st;
+    out.close();
 
+    // opening file using constructor and reading from it
+    string st2;
+    ifstream in("demo1.txt");        //read operation
+    getline(in,st2);
+    cout<<st2;
+    in.close();
+
+    return 0;
+}
